092141992e6e00bb9c79cfdbe851dc612104bce4
htakemur <htakemur@stanford.edu>
2015-09-08 11:28:08 +0900

    Modified code for identifying the ILF

--- scripts/fascicleidentification/s_identifyLHILF.m
+++ scripts/fascicleidentification/s_identifyLHILF.m
@@ -1,52 +1,50 @@
-function s_identifyLHILF(feFileToLoad, fname, dtFile)
+function s_identifyLHILF(feFileToLoad, fname, roiDir, roiNames, roiOperations)
 
-% This script identify the ILF (in Left hemisphere) from fe structure using AFQ code.
-% This script assumes that AFQ ROIs are already determined through AFQ
-% pipelines. See http://white.stanford.edu/newlm/index.php/AFQ
+% This script identify the left ILF (Inferior Longitudinal Fasciculus) from fe structure using waypoint ROIs.
+% In most cases, waypoint ROI can be defined using AFQ toolbox (See http://white.stanford.edu/newlm/index.php/AFQ), but for some dataset manual % % modification is necessary to get proper results (e.g. the brain which has largely different size and organization from MNI template)
 %
 % INPUT:
 % feFileToLoad: a string of full path to .mat file containing fe structure
 % fname: The string of filename for output fiber file (.pdb or .mat format)
-% dtFile: A full path to dt6.mat file of the subject
+% roiDir: A full path to the directory where ROI .mat files are stored
+% roinames: The string of the name of the Waypoint ROIs
+% roiOperations: The operation of ROI waypoint segmentation ('and', 'not'
+% or 'endpoints'
 %
-% feFileToLoad = {'S1_STN96_LH_ILF_SPC_0p25_fe.mat','S1_STN96_LH_ILF_SPC_2_fe.mat'};
-% fname = {'LH_ILF_SPC_0p25.pdb','LH_ILF_SPC_2.pdb'}
-% dtFile = '/data//S1/S1_STN96_data/diffusion_dt6/dt6.mat';
+% feFileToLoad = 'S1_STN96_LH_ILF_SPC_0p25_fe.mat';
+% fname = 'LH_Ufiber_SPC_0p25.mat';
+% roiDir = '/data/STN96/S1/S1_STN96_data/ROIs/WaypointROIs';
+% roiNames = {'S1_LH_ILF_Anterior.mat','S1_LH_ILF_Posterior.mat'};
+% roiOperations = {'and','and'}
+% (C) Hiromasa Takemura, Stanford VISTA Lab/CiNet 2015
 
-% (C) Hiromasa Takemura, Stanford VISTA Lab 2014
-
-for i =  1:length(feFileToLoad)
-    % Load fe structure
-    fprintf('Loading Connectome ...\n')    
-    load(feFileToLoad{i});
-    
-    % Load fascicle weights
-    fweight = feGet(fe,'fiber weights');
-    fweight(fweight > 0) = 1;
-    
-    % Remove zero-weight fascicles and make new fe structure without zero
-    % fascicles
-    fprintf('Removing zero-weight fibers ...\n') 
-    [feculled, removedFibers] = feConnectomeReduceFibers(fe, logical(fweight));
-
-    % Get fascicle coordinates information
-    fg = feGet(feculled,'fibers acpc');
-
-    % Run AFQ Segmentation based on Mori ROI atlase in AFQ toolbox
-    fprintf('Segmenting the Tract from Connectome ...\n')
-    fg_classified = AFQ_SegmentFiberGroups(dtFile, fg);
-    
-    fgnew = fg_classified;
-    
-    % Select Left ILF: No 13 in AFQ fascicle index
-    fgnew.fibers = fg_classified.fibers(fg_classified.subgroup == 13);
-    
-    % Exclude topological outlier
-    fprintf('Excluding outliers from Fasciculus ...\n')
-    [fgsegment, keepFascicle3] = mbaComputeFibersOutliers(fgnew,3,3,25);
-    
-    % Save file
-    fgsegment.name = fname{i};
-    fgWrite(fgsegment);
-    
+for iroi = 1:length(roiNames)
+    rois{iroi} = fullfile(roiDir,roiNames{iroi});
 end
+
+% Load fe structure
+disp('loading the LiFE structure...')
+if ischar(feFileToLoad)
+    fprintf('Loading %s ...\n',feFileToLoad)
+    load(feFileToLoad);
+else
+    fe  =feFileToLoad;
+    clear feFileToLoad;
+end
+
+% Extract the fiber group from the FE structure
+fg = feGet(fe,'fibers acpc');
+fweight = feGet(fe,'fiber weights');
+fweight(fweight > 0) = 1;
+
+% Remove fascicles with zero-weights
+fgkeep = fgExtract(fg, transpose(logical(fweight)), 'keep');
+
+% Segment fascicles based on waypoint ROIs
+[fgsegment, keepFascicles] = feSegmentFascicleFromConnectome(fgkeep, rois, roiOperations, 'prob connectome');
+
+% Exclude topological outliers using mba code
+[fgsegment3, keepFascicle3] = mbaComputeFibersOutliers(fgsegment,3,3,40);
+
+% Save files
+fgWrite(fgsegment3, fname);
